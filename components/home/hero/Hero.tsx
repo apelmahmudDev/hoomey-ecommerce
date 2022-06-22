b5a66a3f6007a55625f4d14db410e6923629388c
DHN Chandan <php.chandan@gmail.com>
2022-06-22 12:19:46 +0600

    refactor: :art: Seperate data

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -3,41 +3,7 @@ import { Box, Grid, Avatar, Typography, Container, Button } from "@mui/material"
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper";
 import { useStyles } from "./styled";
-
-// heroData
-const heroData: {
-	image: string;
-	title: string;
-	description: string;
-}[] = [
-	{
-		image: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-		title: "Brown realistic sofa with decorative cushions",
-		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-		Veniam assumenda consequatur esse nisi dolorem repellendus,
-		autem inventore eius qui eaque molestiae tempore
-		perspiciatis ipsum corrupti tenetur excepturi voluptate ut
-		illo.`,
-	},
-	{
-		image: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-		title: "Two-seater gray sofa with two cushions",
-		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Veniam assumenda consequatur esse nisi dolorem repellendus,
-					autem inventore eius qui eaque molestiae tempore
-					perspiciatis ipsum corrupti tenetur excepturi voluptate ut
-					illo.`,
-	},
-	{
-		image: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-		title: "Chair and pillow",
-		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Veniam assumenda consequatur esse nisi dolorem repellendus,
-					autem inventore eius qui eaque molestiae tempore
-					perspiciatis ipsum corrupti tenetur excepturi voluptate ut
-					illo.`,
-	},
-];
+import { heroData } from "./data";
 
 const Hero: FC = () => {
 	const classes = useStyles();
