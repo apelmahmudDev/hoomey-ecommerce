fde5eabba9f97b948c1bc016f639c97fa90dceaa
Mahedi Hasan <sajib001@gmail.com>
2022-07-19 11:48:24 +0600

    feat: popup fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 
 import { Hero, ForFemale, ForMen, ForKids, LatestProducts, ProvideQuality, Collections } from "../components/home";
-import { Commitment, Footer, Navbar, NewsLetter, TrackOrder } from "../components/common";
+import { Commitment, Footer, Navbar, NewsLetter, RatingPopup } from "../components/common";
 
 const Home: NextPage = () => {
 	return (
@@ -33,12 +33,12 @@ const Home: NextPage = () => {
 			{/* popup & others section area*/}
 			{/* <NewsLetterPopup /> */}
 			{/* <CookiePopup /> */}
-			{/* <RatingPopup /> */}
+			<RatingPopup />
 			{/* <SignUp /> */}
 			{/* <SignIn /> */}
 			{/* <PasswordChange /> */}
 			{/* <ForgetPassword /> */}
-			<TrackOrder />
+			{/* <TrackOrder /> */}
 		</div>
 	);
 };
