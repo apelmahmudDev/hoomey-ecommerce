1484890b19bf804a377ff7333700fcae18f26c3b
Mahedi Hasan <sajib001@gmail.com>
2022-07-20 14:01:53 +0600

    refactor: ♻️  refactor code

--- components/accountSettings/index.ts
+++ components/accountSettings/index.ts
@@ -1,6 +1,6 @@
-export { default as MyAccount } from "./myAccount";
-export { default as ChangePassword } from "./changePassword";
-export { default as AddressBook } from "./addressBook";
 export { default as MyOrders } from "./myOrders";
+export { default as MyAccount } from "./myAccount";
 export { default as MyWishlist } from "./myWishlist";
+export { default as AddressBook } from "./addressBook";
 export { default as PaymentMethods } from "./paymentMethods";
+export { default as ChangePassword } from "./changePassword";
