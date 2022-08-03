af99067029a57db020a7f8a2fbc56a2a3a7d247b
Mahedi Hasan <sajib001@gmail.com>
2022-08-03 17:28:05 +0600

    fix: link of gym

--- components/gymAndSports/collections/data.ts
+++ components/gymAndSports/collections/data.ts
@@ -2,11 +2,11 @@ import { ICollection } from "../../../types/collection";
 import { IMAGES } from "../../../uiElements";
 
 export const itemData: ICollection[] = [
-	{ image: IMAGES.Gym1Img, content: "Trending" },
-	{ image: IMAGES.Gym2Img, content: "Discounts" },
-	{ image: IMAGES.Gym3Img, content: "Women Outfits" },
-	{ image: IMAGES.Gym4Img, content: "Men Outfits" },
-	{ image: IMAGES.Gym5Img, content: "Sports Items" },
-	{ image: IMAGES.Gym6Img, content: "Gym Weights" },
-	{ image: IMAGES.Gym7Img, content: "Accessories" },
+	{ image: IMAGES.Gym1Img, content: "Trending", link: "/gym" },
+	{ image: IMAGES.Gym2Img, content: "Discounts", link: "/gym" },
+	{ image: IMAGES.Gym3Img, content: "Women Outfits", link: "/gym" },
+	{ image: IMAGES.Gym4Img, content: "Men Outfits", link: "/gym" },
+	{ image: IMAGES.Gym5Img, content: "Sports Items", link: "/gym" },
+	{ image: IMAGES.Gym6Img, content: "Gym Weights", link: "/gym" },
+	{ image: IMAGES.Gym7Img, content: "Accessories", link: "/gym" },
 ];
