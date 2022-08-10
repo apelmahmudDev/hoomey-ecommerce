0d76b88e857eb3c7dc6320871b472d1aeb5ebbe3
Mahedi Hasan <sajib001@gmail.com>
2022-08-10 08:47:26 +0600

    feat: order palced email

--- components/emailTemplate/orderPlacedEmail/OrderPlacedEmail.tsx
+++ components/emailTemplate/orderPlacedEmail/OrderPlacedEmail.tsx
@@ -1,32 +1,103 @@
-import { Box, Typography, Button } from "@mui/material";
+import { styled } from "@mui/system";
+import { Box, Typography, Button, Divider } from "@mui/material";
+import Image from "next/image";
+import { IMAGES } from "../../../uiElements";
 import EmailLayout from "../common/layout";
+import { ProductCircularColor } from "../../styledComponents";
+
+const FlexStack = styled("div")({
+	display: "flex",
+	margin: "7px 0",
+	justifyContent: "space-between",
+});
 
 const OrderPlacedEmail = () => {
 	return (
 		<EmailLayout>
 			<Box sx={{ border: 1, borderColor: "#B0B0B0", p: 3 }}>
 				<Typography>
-					<strong>Nice to meet you! 👋</strong>
+					Hello
+					<strong> John Doe 👋</strong>
 				</Typography>
-				<Typography sx={{ my: 2.5 }}>
-					Hi <strong>Jhon Doe</strong> ,
-				</Typography>
+
 				<Typography sx={{ mt: 2 }}>
-					Thank you for believing in our dream! Our mission is to match our customers with the right products.
-					Start exploring with us!
+					Thank you for your purchase! We’ve received your order, we will notify you when it has been sent.
 				</Typography>
+
+				{/* product */}
+				<Box>
+					<Typography fontWeight="600" sx={{ my: 3 }}>
+						Order Details:
+					</Typography>
+
+					{[...Array(1)].map((item, idx) => (
+						<Box key={idx}>
+							<Box sx={{ display: "flex", gap: 3 }}>
+								<Image
+									src={IMAGES.WhiteTshirtImg}
+									alt="product"
+									height={65}
+									width={65}
+									objectFit="cover"
+								/>
+								<Box sx={{ width: "100%" }}>
+									<FlexStack>
+										<Typography fontWeight="600">White Cotton Sweatshirt</Typography>
+										<Typography>$50.00</Typography>
+									</FlexStack>
+									<FlexStack>
+										<Typography color="text.secondary">Quantity</Typography>
+										<Typography color="text.secondary">1</Typography>
+									</FlexStack>
+									<FlexStack>
+										<Typography color="text.secondary">Color</Typography>
+										<ProductCircularColor color="#ddd" />
+									</FlexStack>
+									<FlexStack>
+										<Typography color="text.secondary">Size</Typography>
+										<Typography color="text.secondary">M</Typography>
+									</FlexStack>
+								</Box>
+							</Box>
+							<Divider sx={{ my: 2, borderColor: "#EEEEEE" }} />
+						</Box>
+					))}
+				</Box>
+
+				{/* subtotal */}
+				<Box>
+					<FlexStack>
+						<Typography>Subtotal</Typography>
+						<Typography fontWeight="600">$50.00</Typography>
+					</FlexStack>
+					<FlexStack>
+						<Typography>USPS Priority 2-3 Days</Typography>
+						<Typography fontWeight="600">$5.00</Typography>
+					</FlexStack>
+					<FlexStack>
+						<Typography>Tax</Typography>
+						<Typography fontWeight="600">$0.00</Typography>
+					</FlexStack>
+
+					<Divider sx={{ my: 2, borderColor: "#EEEEEE" }} />
+
+					<FlexStack>
+						<Typography>
+							<strong>Total</strong>
+						</Typography>
+						<Typography fontWeight="600">$2,250</Typography>
+					</FlexStack>
+				</Box>
+
 				<Box sx={{ textAlign: "center" }}>
 					<Button
 						size="large"
 						variant="contained"
 						sx={{ my: 2.5, minWidth: 228, minHeight: 56, fontSize: 16, fontWeight: 500 }}
 					>
-						Let’s go
+						Shop Again
 					</Button>
 				</Box>
-				<Typography textAlign="center" variant="body2" color="text.secondary">
-					If you did not sign up for this account, you can ignore this email.
-				</Typography>
 			</Box>
 		</EmailLayout>
 	);
