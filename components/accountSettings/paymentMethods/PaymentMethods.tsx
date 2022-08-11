e96d5bcb48c6acb13b206f56d19a9799e3c6d25b
Mahedi Hasan <sajib001@gmail.com>
2022-08-11 08:49:31 +0600

    feat: ✨ add checkbox for select payemnts

--- components/accountSettings/paymentMethods/PaymentMethods.tsx
+++ components/accountSettings/paymentMethods/PaymentMethods.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography, IconButton } from "@mui/material";
+import { Box, Button, Typography, IconButton, FormControlLabel, Checkbox } from "@mui/material";
 import { FlexStack, SettingsDivider, TitleFlexStack, TitleText } from "../styledComponents";
 import { PaymentMethodsSvg, DeleteSvg, MasterCardSvg, VisaSvg } from "../../icons";
 import PaymentPopup from "./PaymentPopup";
@@ -6,9 +6,15 @@ import { useState } from "react";
 
 const AddressBook = () => {
 	const [isPaymentMethodOpen, setIsPaymentMethodOpen] = useState(false);
+	const [isDefaultChecked, setIsDefaultChecked] = useState(true);
 
 	const handleTogglePopup = (boolean: boolean) => {
 		setIsPaymentMethodOpen(boolean);
+	};
+
+	// handle default payment check
+	const handleDefaultCheckChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setIsDefaultChecked(event.target.checked);
 	};
 
 	return (
@@ -27,8 +33,12 @@ const AddressBook = () => {
 
 			{/* payment methods card */}
 			{[
-				{ card: <MasterCardSvg height={30} width={50} />, aboutCard: "This is your default payment method" },
-				{ card: <VisaSvg height={30} width={50} />, aboutCard: "This card has been expired" },
+				{
+					card: <MasterCardSvg height={30} width={50} />,
+					aboutCard: "This is your default payment method",
+					checked: true,
+				},
+				{ card: <VisaSvg height={30} width={50} />, aboutCard: "This card has been expired", checked: false },
 			].map((item, idx) => (
 				<Box key={idx} sx={{ position: "relative", bgcolor: "common.white", p: 2.5, my: 2.5 }}>
 					<Box sx={{ mr: 7, display: "flex", gap: 2.5 }}>
@@ -51,6 +61,12 @@ const AddressBook = () => {
 							<Typography sx={{ color: "#9E9E9E", fontSize: 12, mt: 3 }}>{item.aboutCard}</Typography>
 						</Box>
 					</Box>
+
+					<FormControlLabel
+						sx={{ mt: 2 }}
+						label="Default Payment Methods"
+						control={<Checkbox defaultChecked={item.checked} onChange={handleDefaultCheckChange} />}
+					/>
 
 					{/* action on right corner */}
 					<Box sx={{ position: "absolute", top: 20, right: 20 }}>
