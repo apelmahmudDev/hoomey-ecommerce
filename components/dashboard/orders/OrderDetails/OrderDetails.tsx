c6d926deef5619d07a5220e816823863658d0542
Mahedi Hasan <sajib001@gmail.com>
2022-08-22 13:06:32 +0600

    feat: ✅ order details layout ready to go forward

--- components/dashboard/orders/OrderDetails/OrderDetails.tsx
+++ components/dashboard/orders/OrderDetails/OrderDetails.tsx
@@ -1,17 +1,48 @@
 import { ORDER_TABS } from "..";
 import { BackIconButton } from "../../../ui";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid, Stack } from "@mui/material";
 import { ITabChange } from "../../../../types/tab-change";
+import TotalItem from "./TotalItem";
+import DeliveryDetails from "./DeliveryDetails";
+import PaymentDetails from "./PaymentDetails";
+import { StatusText } from "../../components/styledComponents";
 
 const OrderDetails = ({ handleTabChange }: ITabChange) => {
 	return (
 		<Box my={2.5}>
 			{/* back to order tab */}
 			<Box mb={3.5}>
-				<BackIconButton onClick={() => handleTabChange(ORDER_TABS.ROOT)}>Order</BackIconButton>
+				<BackIconButton onClick={() => handleTabChange(ORDER_TABS.ROOT)}>Order Details</BackIconButton>
 			</Box>
 
-			{/*  */}
+			<Box component="section">
+				<Stack sx={{ mb: 1.5 }} direction="row" spacing={3.75} justifyContent="flex-end">
+					<Typography fontWeight="medium">Delivery Status</Typography>
+					<StatusText fs={18} status="Delivered">
+						Delivered
+					</StatusText>
+				</Stack>
+
+				{/*  order details, payment Details & total items */}
+				<Grid container spacing={2.5}>
+					{/* order details & payment details */}
+					<Grid item xs={12} md={6}>
+						<Grid container spacing={2.5}>
+							<Grid item xs={12}>
+								<DeliveryDetails />
+							</Grid>
+							<Grid item xs={12}>
+								<PaymentDetails />
+							</Grid>
+						</Grid>
+					</Grid>
+
+					{/* total items */}
+					<Grid item xs={12} md={6}>
+						<TotalItem />
+					</Grid>
+				</Grid>
+			</Box>
 		</Box>
 	);
 };
