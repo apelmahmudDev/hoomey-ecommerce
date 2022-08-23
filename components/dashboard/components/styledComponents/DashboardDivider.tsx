af44fcc8eb47277b29ce86b2adc6183d50acb540
Mahedi Hasan <sajib001@gmail.com>
2022-08-23 14:35:33 +0600

    feat: add customer section of abandoned cart details

---
+++ components/dashboard/components/styledComponents/DashboardDivider.tsx
@@ -0,0 +1,9 @@
+import { Divider } from "@mui/material";
+import { styled } from "@mui/system";
+
+const DashboardDivider = styled(Divider)({
+	opacity: 0.05,
+	border: "1.05635px solid #292929",
+});
+
+export default DashboardDivider;
