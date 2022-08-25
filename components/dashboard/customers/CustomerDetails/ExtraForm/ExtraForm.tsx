1af83a4587a46270d8182d973b73df253fb1579a
Mahedi Hasan <sajib001@gmail.com>
2022-08-25 12:16:59 +0600

    feat: add extram form of customers

--- components/dashboard/customers/CustomerDetails/ExtraForm/ExtraForm.tsx
+++ components/dashboard/customers/CustomerDetails/ExtraForm/ExtraForm.tsx
@@ -1,5 +1,48 @@
+import { CardContent, FormControlLabel, Box, Typography, Checkbox } from "@mui/material";
+import { StyledCard, StyledTextField } from "../../../components/styledComponents";
+
 const ExtraForm = () => {
-	return <div>ExtraForm</div>;
+	return (
+		<>
+			{/* Tax Exemptions */}
+			<Box my={2.5}>
+				<StyledCard>
+					<CardContent>
+						<Typography fontWeight="medium" gutterBottom>
+							Tax Exemptions
+						</Typography>
+						<FormControlLabel label="Collect Tax" control={<Checkbox defaultChecked />} />
+					</CardContent>
+				</StyledCard>
+			</Box>
+
+			{/* Marketing Status */}
+			<Box my={2.5}>
+				<StyledCard>
+					<CardContent>
+						<Typography fontWeight="medium" gutterBottom>
+							Marketing Status
+						</Typography>
+						<FormControlLabel label="Email subcription" control={<Checkbox defaultChecked />} />
+						<br />
+						<FormControlLabel label="SMS subcription" control={<Checkbox />} />
+					</CardContent>
+				</StyledCard>
+			</Box>
+
+			{/* Tags */}
+			<Box my={2.5}>
+				<StyledCard>
+					<CardContent>
+						<Typography fontWeight="medium" sx={{ mb: 2.5 }}>
+							Tags
+						</Typography>
+						<StyledTextField size="small" fullWidth placeholder="Find Tags" />
+					</CardContent>
+				</StyledCard>
+			</Box>
+		</>
+	);
 };
 
 export default ExtraForm;
