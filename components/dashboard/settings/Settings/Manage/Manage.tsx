e7f02e2c06ea9507160ce1ef61d625aca08abc11
Mahedi Hasan <sajib001@gmail.com>
2022-08-27 17:17:03 +0600

    feat: ✅ setting root tab

--- components/dashboard/settings/Settings/Manage/Manage.tsx
+++ components/dashboard/settings/Settings/Manage/Manage.tsx
@@ -1,5 +1,44 @@
+import { CardContent, Typography, Box } from "@mui/material";
+import { StyledCard } from "../../../components/styledComponents";
+import { styled } from "@mui/material/styles";
+
+const FlexStack = styled(Box)({
+	gap: 20,
+	margin: "10px 0",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "space-between",
+});
+
 const Manage = () => {
-	return <div>Manage</div>;
+	return (
+		<StyledCard>
+			<CardContent>
+				<Typography sx={{ mb: 2.5 }} fontWeight="medium">
+					Manage
+				</Typography>
+
+				<FlexStack>
+					<Typography variant="body2">Sending Email</Typography>
+					<Typography variant="body2" color="primary.main" fontWeight="medium">
+						Manage
+					</Typography>
+				</FlexStack>
+				<FlexStack>
+					<Typography variant="body2">Blogs</Typography>
+					<Typography variant="body2" color="primary.main" fontWeight="medium">
+						Manage
+					</Typography>
+				</FlexStack>
+				<FlexStack>
+					<Typography variant="body2">Contact Us / Newsletter</Typography>
+					<Typography variant="body2" color="primary.main" fontWeight="medium">
+						Manage
+					</Typography>
+				</FlexStack>
+			</CardContent>
+		</StyledCard>
+	);
 };
 
 export default Manage;
--- components/dashboard/settings/Settings/MegaMenu/MegaMenu.tsx
+++ components/dashboard/settings/Settings/MegaMenu/MegaMenu.tsx
@@ -1,5 +1,44 @@
+import { CardContent, Typography, Box } from "@mui/material";
+import { StyledCard } from "../../../components/styledComponents";
+import { styled } from "@mui/material/styles";
+
+const FlexStack = styled(Box)({
+	gap: 20,
+	margin: "10px 0",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "space-between",
+});
+
 const MegaMenu = () => {
-	return <div>MegaMenu</div>;
+	return (
+		<StyledCard>
+			<CardContent>
+				<Typography sx={{ mb: 2.5 }} fontWeight="medium">
+					Mega Menu
+				</Typography>
+
+				<FlexStack>
+					<Typography variant="body2">Men</Typography>
+					<Typography variant="body2" color="primary.main" fontWeight="medium">
+						Customize
+					</Typography>
+				</FlexStack>
+				<FlexStack>
+					<Typography variant="body2">Women</Typography>
+					<Typography variant="body2" color="primary.main" fontWeight="medium">
+						Customize
+					</Typography>
+				</FlexStack>
+				<FlexStack>
+					<Typography variant="body2">Kids</Typography>
+					<Typography variant="body2" color="primary.main" fontWeight="medium">
+						Customize
+					</Typography>
+				</FlexStack>
+			</CardContent>
+		</StyledCard>
+	);
 };
 
 export default MegaMenu;
