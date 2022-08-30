33b01b3390abd6cf07fbc4010fb96195bdafac3f
Mahedi Hasan <sajib001@gmail.com>
2022-08-30 12:56:26 +0600

    feat: change password form secuired by validation

--- components/accountSettings/changePassword/ChangePassword.tsx
+++ components/accountSettings/changePassword/ChangePassword.tsx
@@ -1,35 +1,58 @@
-import { Box, Button, OutlinedInput } from "@mui/material";
-import { useState } from "react";
+import { Box, Button, TextField, LinearProgress, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
 import { Label, SettingsDivider, TitleFlexStack, TitleText } from "../styledComponents";
 import { ChangePassSvg } from "../../icons";
 import { InputAdornmentElPass } from "../../ui";
 
-interface State {
+// react-hook-form
+import { useForm, SubmitHandler } from "react-hook-form";
+import { isStrongPassword } from "../../../utils/validations";
+
+interface Inputs {
 	currentPassword: string;
 	newPassword: string;
 	reEnterNewPassword: string;
+}
+
+interface State {
 	showCurrentPassword: boolean;
 	showNewPassword: boolean;
 	showReEnterPassword: boolean;
 }
 
 const ChangePassword = () => {
+	const {
+		watch,
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>();
+
+	const [isStrongPass, setIsStrongPass] = useState(false);
 	const [values, setValues] = useState<State>({
-		currentPassword: "",
-		newPassword: "",
-		reEnterNewPassword: "",
 		showCurrentPassword: false,
 		showNewPassword: false,
 		showReEnterPassword: false,
 	});
 
-	const handleChange = (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
-		setValues({ ...values, [prop]: event.target.value });
-	};
-
+	// password visibility handler
 	const handleClickShowPassword = (props: { key: string; value: boolean }) => {
 		setValues({ ...values, [props.key]: !props.value });
 	};
+
+	// handle form submit
+	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+	// password strong label check 🔐
+	useEffect(() => {
+		if (watch("newPassword")) {
+			if (isStrongPassword(watch("newPassword"))) {
+				setIsStrongPass(true);
+			} else {
+				setIsStrongPass(false);
+			}
+		}
+	}, [watch("newPassword")]);
 
 	return (
 		<Box>
@@ -40,6 +63,8 @@ const ChangePassword = () => {
 			<SettingsDivider />
 			<Box
 				component="form"
+				autoComplete="off"
+				onSubmit={handleSubmit(onSubmit)}
 				sx={{
 					"& .MuiTextField-root": { bgcolor: "#fff" },
 				}}
@@ -47,64 +72,87 @@ const ChangePassword = () => {
 				{/* current password */}
 				<Box my={2.5}>
 					<Label>Current Password</Label>
-					<OutlinedInput
+					<TextField
 						fullWidth
+						error={errors.currentPassword ? true : false}
+						{...register("currentPassword", { required: true })}
 						type={values.showCurrentPassword ? "text" : "password"}
-						value={values.currentPassword}
-						onChange={handleChange("currentPassword")}
-						endAdornment={
-							<InputAdornmentElPass
-								isShowing={values.showCurrentPassword}
-								onClick={() =>
-									handleClickShowPassword({
-										key: "showCurrentPassword",
-										value: values.showCurrentPassword,
-									})
-								}
-							/>
-						}
+						helperText={errors.currentPassword && "Current password is required."}
+						InputProps={{
+							endAdornment: (
+								<InputAdornmentElPass
+									isShowing={values.showCurrentPassword}
+									onClick={() =>
+										handleClickShowPassword({
+											key: "showCurrentPassword",
+											value: values.showCurrentPassword,
+										})
+									}
+								/>
+							),
+						}}
 					/>
 				</Box>
+
 				{/* new password */}
 				<Box my={2.5}>
 					<Label>New Password</Label>
-					<OutlinedInput
+					<TextField
 						fullWidth
+						error={errors.newPassword ? true : false}
+						{...register("newPassword", {
+							required: "New Password is required",
+							minLength: { value: 8, message: "Password must be at least 8 characters" },
+						})}
 						type={values.showNewPassword ? "text" : "password"}
-						value={values.newPassword}
-						onChange={handleChange("newPassword")}
-						endAdornment={
-							<InputAdornmentElPass
-								isShowing={values.showNewPassword}
-								onClick={() =>
-									handleClickShowPassword({
-										key: "showNewPassword",
-										value: values.showNewPassword,
-									})
-								}
-							/>
-						}
+						helperText={errors.newPassword && errors.newPassword.message}
+						InputProps={{
+							endAdornment: (
+								<InputAdornmentElPass
+									isShowing={values.showNewPassword}
+									onClick={() =>
+										handleClickShowPassword({
+											key: "showNewPassword",
+											value: values.showNewPassword,
+										})
+									}
+								/>
+							),
+						}}
 					/>
+					{/* password label message */}
+					{watch("newPassword")?.length ? (
+						<Box sx={{ width: "100%", mt: 1.25 }}>
+							<LinearProgress variant="determinate" value={isStrongPass ? 100 : 50} />
+							<Typography textAlign="right" variant="body2" color="primary">
+								{isStrongPass ? "Strong Password" : "Weak Password"}
+							</Typography>
+						</Box>
+					) : null}
 				</Box>
+
 				{/* Re-Enter New Password */}
 				<Box my={2.5}>
 					<Label>Re-Enter New Password</Label>
-					<OutlinedInput
+					<TextField
 						fullWidth
+						error={errors.reEnterNewPassword ? true : false}
+						{...register("reEnterNewPassword", { required: true })}
 						type={values.showReEnterPassword ? "text" : "password"}
-						value={values.reEnterNewPassword}
-						onChange={handleChange("reEnterNewPassword")}
-						endAdornment={
-							<InputAdornmentElPass
-								isShowing={values.showReEnterPassword}
-								onClick={() =>
-									handleClickShowPassword({
-										key: "showReEnterPassword",
-										value: values.showReEnterPassword,
-									})
-								}
-							/>
-						}
+						helperText={errors.reEnterNewPassword && "Please, re-enter password & need to match"}
+						InputProps={{
+							endAdornment: (
+								<InputAdornmentElPass
+									isShowing={values.showReEnterPassword}
+									onClick={() =>
+										handleClickShowPassword({
+											key: "showReEnterPassword",
+											value: values.showReEnterPassword,
+										})
+									}
+								/>
+							),
+						}}
 					/>
 				</Box>
 
