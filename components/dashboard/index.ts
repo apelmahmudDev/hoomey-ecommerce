30cb0fd3999a1360f7b67ad347beb2c8acd51e65
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2022-08-30 19:09:29 +0600

    refactor: notification initialized

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -1,7 +1,8 @@
+export { default as Analytics } from "./analytics";
+export { default as Customers } from "./customers";
 export { default as Dashboard } from "./dashboard";
+export { default as Notification } from "./notification";
 export { default as Orders } from "./orders";
 export { default as Products } from "./products";
 export { default as Reviews } from "./reviews";
-export { default as Customers } from "./customers";
-export { default as Analytics } from "./analytics";
 export { default as Settings } from "./settings";
---
+++ components/dashboard/notification/Notification.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+const Notification = () => {
+	return (
+		<div>
+			<Typography sx={{ mb: 4, mt: 2 }} variant="h6" fontWeight={600}>
+				Notifications & Recent Activity
+			</Typography>
+		</div>
+	);
+};
+
+export default Notification;
