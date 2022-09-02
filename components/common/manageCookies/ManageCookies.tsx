2151d0a8fb42088a489f9c324b6f1ecb070c5d4c
Mahedi Hasan <sajib001@gmail.com>
2022-09-02 16:07:55 +0600

    feat: ✨ set transition in cookie manage

--- components/common/manageCookies/ManageCookies.tsx
+++ components/common/manageCookies/ManageCookies.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography, Collapse } from "@mui/material";
 import { FC, useState } from "react";
 import CookiePopup from "./CookiePopup";
 
@@ -10,58 +10,66 @@ const ManageCookies: FC = () => {
 		setIsCookieOpen(boolean);
 	};
 
+	// hangle cookie accept
+	const handleAcceptCookies = () => {
+		setIsAccept(true);
+	};
+
 	return (
-		<Box sx={{ background: "#1F1F1F", py: 1.8, display: isAccept ? "none" : "block" }}>
-			<Container maxWidth="lg">
-				<Box
-					sx={{
-						display: "flex",
-						alignItems: "center",
-						gap: { xs: 2.5, md: 7 },
-						flexWrap: "wrap",
-						justifyContent: { xs: "center", md: '"flex-end"' },
-						textAlign: "right",
-					}}
-				>
-					<Typography
+		<Collapse in={!isAccept}>
+			<Box sx={{ background: "#1F1F1F", py: 1.8 }}>
+				<Container maxWidth="lg">
+					<Box
 						sx={{
-							fontSize: 12,
-							color: "#fff",
-							textAlign: "center",
-							letterSpacing: "0.04em",
-							fontFamily: "Poppins",
+							display: "flex",
+							alignItems: "center",
+							gap: { xs: 2.5, md: 7 },
+							flexWrap: "wrap",
+							justifyContent: { xs: "center", md: '"flex-end"' },
+							textAlign: "right",
 						}}
 					>
-						This site uses cookies to improve your experience. By clicking, you agree to our Privacy Policy.
-					</Typography>
-					<Box>
-						<Button
-							variant="contained"
-							onClick={() => setIsAccept(true)}
-							sx={{ fontSize: 10, mr: 1, fontFamily: "Poppins", letterSpacing: "0.04em" }}
-						>
-							Accept Cookies
-						</Button>
-						<Button
-							onClick={() => handleTogglePopup(true)}
-							variant="text"
+						<Typography
 							sx={{
-								fontFamily: "Poppins",
-								letterSpacing: "0.04em",
-								textDecoration: "underline",
+								fontSize: 12,
 								color: "#fff",
-								fontSize: 10,
+								textAlign: "center",
+								letterSpacing: "0.04em",
+								fontFamily: "Poppins",
 							}}
 						>
-							Manage Cookies
-						</Button>
+							This site uses cookies to improve your experience. By clicking, you agree to our Privacy
+							Policy.
+						</Typography>
+						<Box>
+							<Button
+								variant="contained"
+								onClick={handleAcceptCookies}
+								sx={{ fontSize: 10, mr: 1, fontFamily: "Poppins", letterSpacing: "0.04em" }}
+							>
+								Accept Cookies
+							</Button>
+							<Button
+								onClick={() => handleTogglePopup(true)}
+								variant="text"
+								sx={{
+									fontFamily: "Poppins",
+									letterSpacing: "0.04em",
+									textDecoration: "underline",
+									color: "#fff",
+									fontSize: 10,
+								}}
+							>
+								Manage Cookies
+							</Button>
+						</Box>
 					</Box>
-				</Box>
-			</Container>
+				</Container>
 
-			{/* manage cookie  - popup*/}
-			<CookiePopup handleToggle={handleTogglePopup} isOpen={isCookieOpen} />
-		</Box>
+				{/* manage cookie  - popup*/}
+				<CookiePopup handleToggle={handleTogglePopup} isOpen={isCookieOpen} />
+			</Box>
+		</Collapse>
 	);
 };
 
