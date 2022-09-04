cab9b66ff8cc954ce2a0b634effc4db04eef4bb2
Mahedi Hasan <sajib001@gmail.com>
2022-09-04 11:46:05 +0600

    fix: 🐛 export component

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -1,6 +1,4 @@
 export { default as Analytics } from "./analytics";
 export { default as Dashboard } from "./dashboard";
 export { default as Notification } from "./notification";
-export { default as Products } from "./products";
 export { default as Reviews } from "./reviews";
-export { default as Settings } from "./settings";
