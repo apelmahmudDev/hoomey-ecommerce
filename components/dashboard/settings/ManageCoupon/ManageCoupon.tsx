7db03e57d9a722fd441367398aeac96088469441
Mahedi Hasan <sajib001@gmail.com>
2022-09-04 09:43:51 +0600

    feat: 🚚 add route for manage-coupon of settings

--- components/dashboard/settings/ManageCoupon/ManageCoupon.tsx
+++ components/dashboard/settings/ManageCoupon/ManageCoupon.tsx
@@ -1,16 +1,14 @@
 import { Box } from "@mui/material";
-import { SETTINGS_TABS } from "../../../../constants/tabs/settings";
-import { useAppDispatch } from "../../../../store/hooks";
-import { tabSwitch } from "../../../../store/slices/settingsSlice";
+import { useRouter } from "next/router";
 import { BackIconButton } from "../../../ui";
 import ManageCouponTable from "./ManageCouponTable";
 
 const ManageCoupon = () => {
-	const dispatch = useAppDispatch();
+	const router = useRouter();
 
 	return (
 		<Box my={2.5}>
-			<BackIconButton onClick={() => dispatch(tabSwitch(SETTINGS_TABS.ROOT))}>Manage Coupon</BackIconButton>
+			<BackIconButton onClick={() => router.back()}>Manage Coupon</BackIconButton>
 
 			{/* others sections */}
 			<Box mt={3.75} component="section">
