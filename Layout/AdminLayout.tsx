fe494463b0508a2fbe7e74bac25e5c17a76161e6
Mahedi Hasan <sajib001@gmail.com>
2022-09-04 08:15:51 +0600

    refactor: ♻️   admin-layout

--- Layout/AdminLayout.tsx
+++ Layout/AdminLayout.tsx
@@ -1,17 +1,19 @@
+import {
+	Box,
+	List,
+	Badge,
+	Drawer,
+	AppBar,
+	Toolbar,
+	ListItem,
+	Typography,
+	IconButton,
+	CssBaseline,
+	ListItemText,
+	ListItemIcon,
+	ListItemButton,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import AppBar from "@mui/material/AppBar";
-import Badge from "@mui/material/Badge";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Head from "next/head";
 import { FC, useState } from "react";
 import { AppAvatar, AppDivider, Search, SearchIconWrapper, StyledInputBase } from "../components/styledComponents";
@@ -67,7 +69,11 @@ const drawerItem = [
 	},
 	{ icon: <BarSvg />, text: "Analytics", link: "/dashboard/analytics" },
 	{ icon: <NotificationsSvg />, text: "Notification", link: "/dashboard/notification" },
-	{ icon: <SettingsSvg />, text: "Settings", link: "/dashboard/settings" },
+	{
+		icon: <SettingsSvg />,
+		text: "Settings",
+		link: `${ROUTING_TREE.DASHBOARD.ROOT}/${ROUTING_TREE.DASHBOARD.SETTINGS.ROOT}`,
+	},
 ];
 
 const AdminLayout: FC<AdminLayoutProps> = ({ children, window }) => {
