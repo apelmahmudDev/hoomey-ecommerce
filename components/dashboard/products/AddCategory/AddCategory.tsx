7ecac21248884528edb7640db6ba88d1e0fbd1b3
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2022-09-10 14:47:19 +0600

    refactor: add categories added finally
    took a lot of time to do it in child component with  typescript

--- components/dashboard/products/AddCategory/AddCategory.tsx
+++ components/dashboard/products/AddCategory/AddCategory.tsx
@@ -1,6 +1,10 @@
 import { Box, Button, Grid } from "@mui/material";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useCreateCategoryMutation } from "../../../../store/api/categoryApi";
+import { useAppDispatch } from "../../../../store/hooks";
+import { useToastify } from "../../../../store/slices/toastifySlice";
 import { Popup } from "../../../common";
 import { BackIconButton } from "../../../ui";
 import Media from "../AddProduct/Media";
@@ -12,16 +16,43 @@ interface Inputs {
 	name: string;
 	slug: string;
 	description: string;
-	status: boolean;
+	isStatus: string;
 }
 
 const AddCategory = () => {
+	const dispatch = useAppDispatch();
 	const router = useRouter();
+	const [createCategory, { data, isLoading, error }] = useCreateCategoryMutation();
 	const [isAddSubCategoryOpen, setIsAddSubCategoryOpen] = useState(false);
+
+	const { register, handleSubmit } = useForm<Inputs>();
+
+	// handle form submit & user creation
+	const onSubmit: SubmitHandler<Inputs> = (data) => {
+		const { name, description, isStatus } = data;
+
+		// eslint-disable-next-line @typescript-eslint/no-floating-promises
+		createCategory({
+			name: name,
+			slug: `${name.toLowerCase()}`,
+			description: description,
+			status: isStatus === "Active" ? true : false,
+		});
+	};
 
 	const handleAddSubCategoryTogglePopup = (boolean: boolean) => {
 		setIsAddSubCategoryOpen(boolean);
 	};
+
+	//console.log("category", data);
+	// console.log(Boolean(error));
+
+	//  notifications
+	useEffect(() => {
+		if (error) dispatch(useToastify({ desc: "Category create failed.", severity: "error" }));
+		if (data) dispatch(useToastify({ desc: "Category create successful.", severity: "success" }));
+	}, [dispatch, error, data]);
+
 	return (
 		<div>
 			<Box
@@ -37,53 +68,59 @@ const AddCategory = () => {
 			>
 				<BackIconButton onClick={() => router.back()}>Add Category</BackIconButton>
 
-				<Grid container spacing={2.5}>
-					<Grid item xs={12} md={7}>
-						<Grid container spacing={2.5}>
-							<Grid item xs={12}>
-								<Title />
+				<Box component="form" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+					<Grid container spacing={2.5}>
+						<Grid item xs={12} md={7}>
+							<Grid container spacing={2.5}>
+								<Grid item xs={12}>
+									<Title register={register} />
+								</Grid>
+								<Grid item xs={12}>
+									<Media />
+								</Grid>
 							</Grid>
-							<Grid item xs={12}>
-								<Media />
+						</Grid>
+						<Grid item xs={12} md={5}>
+							<Grid container spacing={2.5}>
+								<Grid item xs={12}>
+									<CategoryStatus
+										register={register}
+										handleTogglePopup={handleAddSubCategoryTogglePopup}
+									/>
+								</Grid>
+							</Grid>
+						</Grid>
+						<Grid item xs={12}>
+							<Grid container spacing={2.5}>
+								<Grid item xs={3} />
+								<Grid item xs={6}>
+									<Box sx={{ display: "flex", alignItems: "center", gap: 2.5 }}>
+										<Button
+											sx={{ borderRadius: "5px" }}
+											size="large"
+											fullWidth
+											variant="outlined"
+											color="secondary"
+										>
+											Cancel
+										</Button>
+										<Button
+											sx={{ borderRadius: "5px" }}
+											size="large"
+											fullWidth
+											variant="contained"
+											color="secondary"
+											type="submit"
+										>
+											{isLoading ? "Please wait..." : "Add"}
+										</Button>
+									</Box>
+								</Grid>
+								<Grid item xs={3} />
 							</Grid>
 						</Grid>
 					</Grid>
-					<Grid item xs={12} md={5}>
-						<Grid container spacing={2.5}>
-							<Grid item xs={12}>
-								<CategoryStatus handleTogglePopup={handleAddSubCategoryTogglePopup} />
-							</Grid>
-						</Grid>
-					</Grid>
-					<Grid item xs={12}>
-						<Grid container spacing={2.5}>
-							<Grid item xs={3} />
-							<Grid item xs={6}>
-								<Box sx={{ display: "flex", alignItems: "center", gap: 2.5 }}>
-									<Button
-										sx={{ borderRadius: "5px" }}
-										size="large"
-										fullWidth
-										variant="outlined"
-										color="secondary"
-									>
-										Cancel
-									</Button>
-									<Button
-										sx={{ borderRadius: "5px" }}
-										size="large"
-										fullWidth
-										variant="contained"
-										color="secondary"
-									>
-										Add
-									</Button>
-								</Box>
-							</Grid>
-							<Grid item xs={3} />
-						</Grid>
-					</Grid>
-				</Grid>
+				</Box>
 
 				<Popup
 					isOpen={isAddSubCategoryOpen}
--- components/dashboard/products/AddCategory/Title/Title.tsx
+++ components/dashboard/products/AddCategory/Title/Title.tsx
@@ -1,10 +1,21 @@
 import { Box, CardContent, Typography } from "@mui/material";
 import { Editor } from "@tinymce/tinymce-react";
 import { useRef } from "react";
+import { UseFormRegister } from "react-hook-form";
 
 import { StyledCard, StyledTextField } from "../../../components/styledComponents";
 
-const Title = () => {
+interface Inputs {
+	name: string;
+	slug: string;
+	description: string;
+	isStatus: string;
+}
+interface MyProps {
+	register: UseFormRegister<Inputs>;
+}
+
+const Title = ({ register }: MyProps) => {
 	const editorRef = useRef("test");
 	return (
 		<StyledCard>
@@ -14,7 +25,13 @@ const Title = () => {
 				</Typography>
 
 				<Box component="form" sx={{ display: "flex", alignItems: "center", gap: 1.25 }}>
-					<StyledTextField size="small" fullWidth required placeholder="Men" />
+					<StyledTextField
+						size="small"
+						{...register("name", { required: "This field is required" })}
+						fullWidth
+						required
+						placeholder="Men"
+					/>
 				</Box>
 				<Typography sx={{ fontSize: 18, mt: 1.25, mb: 1.25 }} fontWeight="medium">
 					Description (Optional)
@@ -26,6 +43,7 @@ const Title = () => {
 							apiKey={process.env.NEXT_PUBLIC_TEXT_EDITOR_API_KEY}
 							onInit={(evt, editor) => (editorRef.current = "test")}
 							initialValue="<p>Write description here...</p>"
+							{...register("description")}
 							init={{
 								height: 200,
 								menubar: false,
