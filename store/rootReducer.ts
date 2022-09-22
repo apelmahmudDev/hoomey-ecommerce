9849614d3b9f90b0a0b08cb30584007671e36375
Mahedi Hasan <sajib001@gmail.com>
2022-09-22 14:42:05 +0600

    feat: add reducer

--- store/rootReducer.ts
+++ store/rootReducer.ts
@@ -10,17 +10,21 @@ import { warningSlice } from "./slices/warningSlice";
 import { authApi } from "./api/authApi";
 import { categoryApi } from "./api/categoryApi";
 import { productApi } from "./api/productApi";
+import { tagApi } from "./api/tagApi";
 
 const rootReducer = combineReducers({
 	[authApi.reducerPath]: authApi.reducer,
 	[categoryApi.reducerPath]: categoryApi.reducer,
 	[productApi.reducerPath]: productApi.reducer,
+	[tagApi.reducerPath]: tagApi.reducer,
 
 	// slices
 	[cartSlice.name]: cartSlice.reducer,
 	[colorSlice.name]: colorSlice.reducer,
 	[toastifySlice.name]: toastifySlice.reducer,
 	[warningSlice.name]: warningSlice.reducer,
+
+	// middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
 });
 
 export default rootReducer;
